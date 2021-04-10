0b1b3163bf89a633c0767a21f894ad935bfadfa6
quagsirus <quagsirus@catpowered.net>
2021-04-10 15:46:16 +0100

    add youtube watchtogether

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import {
 
 const helloCommand: ApplicationCommand = {
   name: "hello",
-  description: "Bot will say hello to you!",
+  description: "bot will say hello to you!",
 };
 
 const helloHandler: InteractionHandler = async (
@@ -28,10 +28,49 @@ const helloHandler: InteractionHandler = async (
   };
 };
 
+const ytCommand: ApplicationCommand = {
+  name: "yt",
+  description: "starts a YouTube watch together session",
+  options: [
+    {
+      name: "channel",
+      description: "the voice channel to setup in",
+      type: 7,
+      required: true
+    }
+  ]
+}
+
+const ytHandler: InteractionHandler = async (
+  interaction: Interaction
+): Promise<InteractionResponse> => {
+  const userID = interaction.member.user.id
+
+  const r = await fetch(`https://discord.com/api/v8/channels/${interaction.data!.options![0].value}/invites`, {
+    method: 'POST',
+    // @ts-expect-error (supresses error shown due to use of Cloudflare Workers Secrets)
+    headers: { authorization: `Bot ${botToken}`, 'content-type': 'application/json' },
+    body: JSON.stringify({
+      max_age: 86400,
+      target_type: 2,
+      target_application_id: "755600276941176913"
+    })
+  })
+
+  const invite = await r.json()
+  return {
+    type: InteractionResponseType.ChannelMessageWithSource,
+    data: {
+      content: `[Click to open ${invite.target_application.name} in ${invite.channel!.name}](<https://discord.gg/${invite.code}>)`,
+      allowed_mentions: { parse: [] }
+    }
+  }
+}
+
 const slashCommandHandler = createSlashCommandHandler({
   // @ts-expect-error (supresses error shown due to use of Cloudflare Workers Secrets)
   applicationID: applicationID, applicationSecret: applicationSecret, publicKey: publicKey,
-  commands: [[helloCommand, helloHandler]],
+  commands: [[helloCommand, helloHandler], [ytCommand, ytHandler]],
 });
 
 addEventListener("fetch", (event) => {
